e8813334fba8864378a90d46067250a123d5a614
Tarun Kumar Sukhu <tksukhu@gmail.com>
2017-12-21 14:57:27 +0530

    polyfill cleanup

--- src/polyfills.browser.ts
+++ src/polyfills.browser.ts
@@ -65,10 +65,3 @@ import 'zone.js/dist/zone';
  * Need to import at least one locale-data with intl.
  */
 // import 'intl/locale-data/jsonp/en';
-if ('production' === ENV) {
-  // Production
-
-} else {
-
-  // Development
-}
